ad5eef881fb19240bd8f60d15bd792a31cda22ff
SubiNair <subirnair@gmail.com>
2017-05-02 14:46:35 -0600

    more added to script

--- LectureWeek15.R
+++ LectureWeek15.R
@@ -13,6 +13,8 @@
 #10% growth rate max
 #symmetry in the sense of rates and carrying capacities
 #a12 is the effect of organism 1 on organism 2 aka the equation for n2
+library("deSolve")
+
 r1 <- 0.1
 r2 <- r1
 k1 <- 1000
@@ -28,6 +30,26 @@ times <- seq(0, 400, 0.01)
 #also y is going to have 2 things instead of one since you have n1 and n2
 #y needs to be a vector of length 2
 
+
 #We ned to reference the elements of y as a result
+#how do we define our y value
+competitionODE <- function(t, y, parms) {
+  #to use ode this funciton must have at least three args and they must
+  #be in the order shows, t, y, parms
+  
+  #Here is the ODE
+  dn1dt <- y[1] * parms$r1 * (( parms$k1 - y[1] - parms$a21*y[2]) / parms$k1)
+  dn2dt <- y[2] * parms$r2 * (( parms$k2 - y[2] - parms$a12*y[1]) / parms$k2)
+  #To use ODE the function must return a list
+  #IT HAS TO BE A LIST
+  #but first bind it together as a vector, b/c it can only be a list of 1 object
+  return(list(c(dn1dt, dn2dt)))
+}
 
+parms <- list(r1,k1,a21,r2,k2,a12)
+names(parms) <- c("r1","k1","a21","r2","k2","a12")
 
+#Our y in this situation would be the initial vals
+solution <- ode(y = c(n1init, n2init), times = times, func = competitionODE, parms = parms)
+plot(solution[,1], solution[,2], type = "l", xlab = "time", ylab = "abundance", ylim = c(0,1000), col = "red")
+lines(solution[,1], solution[,3], col = "blue")
