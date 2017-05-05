4c002631a821392d3ac0b8ae557535d9de89f218
SubiNair <subirnair@gmail.com>
2017-05-05 04:07:40 -0600

    updated script

--- Assignment11/independent_proj_script.R
+++ Assignment11/independent_proj_script.R
@@ -16,8 +16,8 @@ ForagingData <- read.csv("Foraging_duration.csv")
 #Fixing the Pollen Data csv file
 #Some of the pollen colours are listed as "?" and so we can replace them with NA
 #And then get rid of anything with an NA
-PollenData$Pollen.Colour <- gsub("?",NA,PollenData$Pollen.Colour, fixed = TRUE)
-PollenData <- na.omit(PollenData)
+#PollenData$Pollen.Colour <- gsub("?",NA,PollenData$Pollen.Colour, fixed = TRUE)
+#PollenData <- na.omit(PollenData)
 
 #Assigning the columns to variables
 pDates <- as.vector(PollenData$Date)
@@ -37,6 +37,8 @@ orderedScoreDates <- sort(scoreDates, decreasing = FALSE)
 #For example for 9-28, we can find all the indeces with 9-28,
 #And then those indeces in the pollen score data set will contain the pollen score for that date for that treatment
 
+possibleScores <- c(0,1,2,3)
+
 #We are creating these distributions by treatment
 #First we will start with the A treatment
 #Which was the control
@@ -46,6 +48,7 @@ orderedScoreDates <- sort(scoreDates, decreasing = FALSE)
 #Then we can use the dpois function or rpois (look into this)
 
 #This collects the indeces for the control and then finds their respective pollen scores
+###A
 ATreatLocs <- which(pTreat %in% "A")
 A_Scores <- pScore[ATreatLocs]
 
@@ -53,12 +56,58 @@ A_Scores <- pScore[ATreatLocs]
 A_lambda <- mean(A_Scores)
 A_dpoisvals <- rep(0, 4)
 
-for (i in seq(0,3)) {
+for (i in possibleScores) {
   A_dpoisvals[i+1] <- dpois(x = i, lambda = A_lambda)
 }
 
 print(A_dpoisvals)
+plot(possibleScores, A_dpoisvals, xlab = "Pollen Score", ylab = "Probability", ylim = c(0,.5))
+Aline <- lm(A_dpoisvals ~ poly(possibleScores, 2))
+summary(Aline)
+
+######B
+BTreatLocs <- which(pTreat %in% "B")
+B_Scores <- pScore[BTreatLocs]
+
+#Calculating the respective lambda
+B_lambda <- mean(B_Scores)
+B_dpoisvals <- rep(0, 4)
+
+for (i in possibleScores) {
+  B_dpoisvals[i+1] <- dpois(x = i, lambda = B_lambda)
+}
+
+print(B_dpoisvals)
+plot(possibleScores, B_dpoisvals, xlab = "Pollen Score", ylab = "Probability", ylim = c(0,.5))
 
 
+####C
+CTreatLocs <- which(pTreat %in% "C")
+C_Scores <- pScore[CTreatLocs]
 
+#Calculating the respective lambda
+C_lambda <- mean(C_Scores)
+C_dpoisvals <- rep(0, 4)
+
+for (i in possibleScores) {
+  C_dpoisvals[i+1] <- dpois(x = i, lambda = C_lambda)
+}
+
+print(C_dpoisvals)
+plot(possibleScores, C_dpoisvals, xlab = "Pollen Score", ylab = "Probability", ylim = c(0,.5))
+
+####D
+DTreatLocs <- which(pTreat %in% "D")
+D_Scores <- pScore[DTreatLocs]
+
+#Calculating the respective lambda
+D_lambda <- mean(D_Scores)
+D_dpoisvals <- rep(0, 4)
+
+for (i in possibleScores) {
+  D_dpoisvals[i+1] <- dpois(x = i, lambda = D_lambda)
+}
+
+print(D_dpoisvals)
+plot(possibleScores, D_dpoisvals, xlab = "Pollen Score", ylab = "Probability", ylim = c(0,.5))
 
